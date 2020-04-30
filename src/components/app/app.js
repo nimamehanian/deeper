04c55ad903c64b9ff24fa3677892675d019ca7e6
Nima Mehanian <nima.mehanian@gmail.com>
2020-04-30 10:56:38 -0700

    Init AuthForm

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,20 +1,21 @@
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
-// import { Route, Switch } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import styled from 'styled-components';
 import GlobalStyles from 'styles/global';
 import { ThemeProvider } from '@material-ui/styles';
 import { createMuiTheme } from '@material-ui/core';
-// import AuthForm from 'components/authForm/authForm';
-import { $sunset } from 'styles/colors';
+import AuthForm from 'components/authForm/authForm';
+import { $sunset, $GOLDEN_RATIO_MICRO } from 'styles/colors';
 
 const Header = styled.div`
   color: #fff;
   background: ${$sunset};
   height: 60px;
   font-size: 30px;
-  font-family: 'Andes Reg';
+  font-family: 'Canela Med';
   line-height: 2;
+  letter-spacing: ${$GOLDEN_RATIO_MICRO}px;
   padding: 0px 0px 0px 8px;
 `;
 
@@ -31,11 +32,14 @@ function App() {
     <div>
       <GlobalStyles />
       <ThemeProvider theme={theme}>
-        <Header>Title</Header>
-        {/* <Switch> */}
-        {/* <Route exact path="/" render={(props) => <AuthForm primaryColor={$olive} {...props} />} /> */}
-        {/* <Route exact path="/pathB" component={ComponentB} /> */}
-        {/* </Switch> */}
+        <Header>
+          <Link to="/">Deeper</Link>
+          <Link to="/auth">SIGN UP</Link>
+        </Header>
+        <Switch>
+          <Route exact path="/auth" render={(props) => <AuthForm {...props} />} />
+          {/* <Route exact path="/pathB" component={ComponentB} /> */}
+        </Switch>
         {/* <Footer /> */}
       </ThemeProvider>
     </div>
---
+++ src/components/authForm/authForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import TextField from '@material-ui/core/TextField';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import { InputAdornment, IconButton } from '@material-ui/core';
+
+function AuthForm() {
+  return (
+    <h1>AuthForm Hello</h1>
+  );
+}
+
+export default AuthForm;
